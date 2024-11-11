a00cdf602f74d7f7b17b521a839f40588ce3e4ec
Esteban Solis <estebansolisd@gmail.com>
2024-11-10 20:36:59 -0600

    Initial Commit

---
+++ Models/Gender.cs
@@ -0,0 +1,9 @@
+namespace person_api_1.Models;
+
+public enum Gender
+{
+    Male,
+    Female,
+    Other,
+    Unspecified
+}
---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+using System;
+using person_api_1.Models;
+
+public class Person
+{
+    public Guid Id { get; set; }
+    public string GivenName { get; set; } = String.Empty;
+    public string Surname { get; set; } = String.Empty;
+    public Gender Gender { get; set; } 
+    public DateTime BirthDate { get; set; }
+    public string BirthLocation { get; set; } = String.Empty;
+    public DateTime? DeathDate { get; set; }  
+    public string? DeathLocation { get; set; }
+
+}
